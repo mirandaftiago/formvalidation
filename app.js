b3bd518ad958da584fbdf1382c86f33e1938bd71
Tiago Miranda <miranda.f.tiago@gmail.com>
2021-03-23 20:12:59 +0000

    validateEmail()

--- app.js
+++ app.js
@@ -28,7 +28,14 @@ function validateZip() {
 }
 
 function validateEmail() {
+  const email = document.getElementById('email');
+  const re = /^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/;
 
+  if(!re.test(email.value)) {
+    email.classList.add('is-invalid');
+  } else {
+    email.classList.remove('is-invalid');
+  }
 }
 
 function validatePhone() {
